4211a2d1f305f153d8d6e6da5e70699e756501bb
Mahdi Pedramrazi <pedramphp@gmail.com>
2016-02-15 17:42:48 -0800

    adding Strings example

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Special Characters
+// \t tabs the result
+// \n new line
+// literals.
+// \t \n string literals.
+print("List:\n1\tApple\n2\tBannanas\n6\tOranges")
+
+// Emoji support - cmd - ctrl - space
+// Emojis are not based on basic character set.
+// Unicode - Industry character standard
+// Swift supports any foriegn language
+print("List:\n1\t🍎\n2\t🍌\n6\t🍊")
