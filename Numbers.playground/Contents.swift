fa045068e5f9efd13e54afc58221f7e38cfd0de5
Mahdi Pedramrazi <pedramphp@gmail.com>
2016-02-15 17:51:17 -0800

    adding Numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let language = "Swift"
+
+// Type Int - Integer - used for whole numbers without decimal
+let year = 2014
+
+// Implicit declaration - Swift intelligently makes decision based on the value that we are assigning it.
+
+// By default is Double
+let version = 1.0 // Double = 15 decimal digits - Float = 7 decimal digits.
+
+var version2: Double = 1.0
+
+var version3: Float = 2.0
+
+// Binary value
+// Boolean or Bool - either true or false
+var isAwesome = true
+
+// explicit declaration
+var isAwesome2: Bool = true
