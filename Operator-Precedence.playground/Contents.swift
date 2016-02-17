4b2b15a6022e56d48bace6442fbbdf54e8d450d5
Mahdi Pedramrazi <pedramphp@gmail.com>
2016-02-16 22:25:06 -0800

    Operator Precedence

---
+++ Operator-Precedence.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// some operator gets higher precedence
+
+// Precedence Level 150 - Left to Right
+// Multipication
+// Division
+// Remainder
+
+// Precedence Level 140 - Left to Right
+// Addition
+// Subtraction
+
+
+var x = 100 + 100 - 5 * 2 / 3 % 7
+
+
+var x2 = 100 + 100 - (((5 * 2) / 3) % 7)
+
