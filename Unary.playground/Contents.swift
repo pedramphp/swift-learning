12be96f8f2dfa9284986bdbde5c90d832af98d81
Mahdi Pedramrazi <pedramphp@gmail.com>
2016-02-16 22:25:41 -0800

    Adding Unary

---
+++ Unary.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var levelScore: Int
+
+levelScore = 0
+
+levelScore = levelScore + 1
+
+// post fix operator
+levelScore++
+
+
+var totalScore = 0
+totalScore = levelScore++
+totalScore
+levelScore
+
+//pre fix operator
+totalScore = ++levelScore
+totalScore
+levelScore
+
+// ++ and -- operators
+//++ increment unary operator
+//-- decrement unary operator
+// - negative operator
+
+levelScore = -levelScore
+
+
+// NOT Operator
+// negates the value
+
+let on = true
+let off = !on
