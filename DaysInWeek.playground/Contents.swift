8c4c1fdf9ddd11552b015fde500ac347c38496a9
Mahdi Pedramrazi <pedramphp@gmail.com>
2016-02-20 18:43:25 -0800

    adding enums

--- DaysInWeek.playground/Contents.swift
+++ DaysInWeek.playground/Contents.swift
@@ -82,6 +82,82 @@ func weekdayOrWeekend(dayOfWeek: Day) -> String {
 weekdayOrWeekend(Day.Monday)
 
 var today = Day.Monday
-today
+
+// it already knows that today is type Day.
+today = .Tuesday
+
+
+//Raw Values
+
+
+enum Day3: Int {
+    case Monday = 1,
+    Tuesday = 2,
+    Wednesday = 3,
+    Thursday = 4,
+    Friday = 5,
+    Saturday = 6,
+    Sunday = 7
+}
+
+func daysTillWeekend(day: Day3) -> Int{
+   return Day3.Saturday.rawValue - day.rawValue
+}
+
+daysTillWeekend(Day3.Monday)
+daysTillWeekend(Day3.Friday)
+
+
+
+// system automatically add values sequential.
+
+enum Day4: Int {
+    case Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+func daysTillWeekend(day: Day4) -> Int{
+    return Day4.Saturday.rawValue - day.rawValue
+}
+
+daysTillWeekend(Day4.Monday)
+daysTillWeekend(Day4.Friday)
+
+// create enum using Raw value
+let firstDayOfWeekend = Day4(rawValue: 1)
+// returns optional.
+print(firstDayOfWeekend)
+
+// returns nil for invalid rawValue
+let tenthtDayOfWeekend = Day4(rawValue: 10)
+
+
+if let firstDayOfWeekend = Day4(rawValue: 1) {
+    daysTillWeekend(firstDayOfWeekend)
+}
+
+//Associated Value
+
+enum Status{
+    case Success(String)
+    case Failure(String)
+}
+
+let downloadStatus = Status.Failure("network connection unavailable")
+
+
+switch downloadStatus {
+case .Success(let success):
+    print(success)
+case .Failure(let failure):
+    print(failure)
+}
+
+
 
 
